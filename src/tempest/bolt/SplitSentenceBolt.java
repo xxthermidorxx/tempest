d17a4c41047b8b37869e23a55f39077b1edf4b5c
Yusuke NISHIOKA <maximilianthermidor765@gmail.com>
2014-04-05 11:45:46 +0900

    add SplitSentenceBolt.java

--- src/tempest/bolt/SplitSentenceBolt.java
+++ src/tempest/bolt/SplitSentenceBolt.java
@@ -1,24 +1,31 @@
 package tempest.bolt;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
 
 import backtype.storm.topology.base.BaseRichBolt;
 import backtype.storm.topology.BasicOutputCollector;
+import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Tuple;
 import backtype.storm.tuple.Values;
 
 public class SplitSentenceBolt extends BaseRichBolt {
 
-    private String _a;
-
     @Override
     public void excute(Tuple tuple, BasicOutputCollector collector) {
+        String sentence = tuple.getString(0);
+        String[] splittedSentence = sentence.split("[\\s]+");
+        collector.emit(new Values(splittedSentence));
     }
 
     @Override
-    public void cleanup() {
+    public Map<String, Object> getComponentConfiguration() {
+        return null;
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+        declarer.declare(new Fields("splitted_sentence"));
     }
 }
