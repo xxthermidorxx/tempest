1a0f28c4d2511a4e79f3b5212dad8a469b522b5f
Yusuke NISHIOKA <maximilianthermidor765@gmail.com>
2014-04-09 19:36:02 +0900

    add LocalCluster for debugging

--- src/tempest/topology/PerceptronTopology.java
+++ src/tempest/topology/PerceptronTopology.java
@@ -2,7 +2,9 @@ package tempest.topology;
 
 import backtype.storm.Config;
 import backtype.storm.StormSubmitter;
+import backtype.storm.LocalCluster;
 import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.tuple.Fields;
 
 import tempest.spout.XmlReadoutSpout;
 import tempest.bolt.SplitSentenceBolt;
@@ -13,14 +15,21 @@ public class PerceptronTopology {
     public static void main(String[] args) throws Exception {
         TopologyBuilder builder = new TopologyBuilder();
 
-        builder.setSpout("spout", new XmlReadoutSpout(), 5);
-        builder.setBolt("split", new SplitSentenceBolt(), 3).shuffleGrouping("spout");
-        builder.setBolt("extract", new ExtractFeatureVectorBolt(), 4).shuffleGrouping("split");
+        builder.setSpout("spout", new XmlReadoutSpout(), 1);
+        builder.setBolt("split", new SplitSentenceBolt(), 1).shuffleGrouping("spout");
+        builder.setBolt("extract", new ExtractFeatureVectorBolt(), 1).shuffleGrouping("split");
 
         Config conf = new Config();
         conf.setDebug(true);
 
-        conf.setNumWorkers(9);
-        StormSubmitter.submitTopology(args[0], conf, builder.createTopology());
+        conf.setNumWorkers(3);
+        if (args != null && args.length > 0) {
+            StormSubmitter.submitTopology(args[0], conf, builder.createTopology());
+        } else {
+            LocalCluster cluster = new LocalCluster();
+            cluster.submitTopology("perceptron", conf, builder.createTopology());
+            Thread.sleep(6000);
+            cluster.shutdown();
+        }
     }
 }
