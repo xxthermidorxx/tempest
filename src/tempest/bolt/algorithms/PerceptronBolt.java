29979c817e8ce7bf48187c23db33ef5ceb926f2d
Yusuke NISHIOKA <maximilianthermidor765@gmail.com>
2014-04-10 13:38:27 +0900

    fix PerceptronBolt.java

--- src/tempest/bolt/algorithms/PerceptronBolt.java
+++ src/tempest/bolt/algorithms/PerceptronBolt.java
@@ -1,59 +1,76 @@
-package tempest.algorithms;
+package tempest.bolt.algorithms;
 
+import java.util.Set;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.Random;
 
-import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.topology.base.BaseBasicBolt;
 import backtype.storm.topology.BasicOutputCollector;
 import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+import backtype.storm.tuple.Fields;
 
-import tempest.FeatureExtraction;
+public class PerceptronBolt extends BaseBasicBolt {
 
-public class PerceptronBolt extends BaseRichBolt {
-
-    private int _dataSize;
-    private int _dimension;
-    private int[] _weightVector;
-    private int[] _categorized;
-    private List<int[]> _featureVectorList;
+    private int __categorized;
+    private Map<String, Integer> __featureVector;
+    private Map<String, Integer> __weightVector;
 
     /**
      * constructor
-     * @param List<int[]> featureVectorList
-     * @return void
+     * @param void
      */
-    public PerceptronBolt(List<int[]> featureVectorList) {
-        _dataSize = featureVectorList.length();
-        _dimension = featureVectorList.get(0).length;
-        _featureVectorList = featureVectorList;
-        _weightVector = new int[ _dimension ];
-
-        Random rnd = new Random();
-        int[] num = {-1, 1};
-        for(int i=0; i<_dimension; i++) {
-            _weightVector[i] = rnd.nextInt(10) - 5;
-            _categorized[i] = num[ rnd.nextInt(1) ];
-        }
+    public PerceptronBolt() {
+        __weightVector = new HashMap<String, Integer>();
     }
 
     /**
-     * calculate the dot product of _weightVector and _featureVector[i]
+     * set weight vector, if it has the kye of the word, set value 0
      * @param void
-     * @return int
+     * @return void
      */
-    public int[] calcDotProduct() {
-        int[] dotProductArray = new int[_dataSize];
-        int sum = 0;
-        for(int i=0; i<_dataSize; i++) {
-            int[] vector = _featureVectorList.get(i);
-            for(int j=0; j<_dimension; j++) {
-                sum += _weightVector[j] * vector[j];
+    private void __setWeightVector() {
+        // 重みベクトルの重みをランダムに生成
+        for (Map.Entry<String, Integer> keyValuePair : __featureVector.entrySet()) {
+            String key = keyValuePair.getKey();
+            if ( !__weightVector.containsKey(key) ) {
+                __weightVector.put( key, 0 );
             }
-            dotProductArray[i] = sum;
         }
-        return dotProductArray;
+
+        // 論文がそのジャンルに分類されるかをランダムに出力
+        int[] num = {-1, 1};
+        Random rnd = new Random();
+        __categorized = num[ rnd.nextInt(1) ];
+    }
+
+    /**
+     * calculate the dot product of __weightVector.values() and __featureVector.values()
+     * @param void
+     * @return int 1 if result is right else 0
+     */
+    private int __calcDotProduct() {
+        int dot_product_sum = 0;
+        for (String key : __featureVector.keySet()) {
+            dot_product_sum += __featureVector.get(key) * __weightVector.get(key);
+        }
+        return (dot_product_sum > 0) ? 1 : 0;
+    }
+
+    /**
+     * update the weight vector if the prediction is wrong
+     * @param int prediction result
+     * @return void
+     */
+    private void __updateWeightVector(int prediction) {
+        for (String key : __featureVector.keySet()) {
+            int updated =  __weightVector.get(key) + prediction * __featureVector.get(key);
+            __weightVector.put(key, updated);
+        }
     }
 
     /**
@@ -61,28 +78,22 @@ public class PerceptronBolt extends BaseRichBolt {
      * @param void
      * @return int
      */
-    public void predict() {
-        int loopCount = 0;
-        while(loopCount < 1000) {
-            int[] dotProductArray = calcDotProduct();
-
-            int predictionMiss = 0;
-            for(int i=0; i<dotProductArray.length; i++) {
-                int prediction = (dotProductArray[i] > 0) ? 1 : -1;
-                if(prediction != _categorized[i]) {
-                    _weightVector += prediction * _weightVector[];    // TODO
-                    predictionMiss++;
-                }
-            }
-            if(predictionMiss == 0) break;
-            loopCount++;
+    private void __predict() {
+        for (int loopCount=0; loopCount<1000; loopCount++) {
+            int prediction = __calcDotProduct();
+            if ( __categorized != prediction ) __updateWeightVector(prediction);
+            else break;
         }
-        return;
     }
 
     @Override
-    public void excute(Tuple tuple, BasicOutputCollector collector) {
-        List<Objec>tuple.getValues();
+    public void execute(Tuple tuple, BasicOutputCollector collector) {
+        /** HACK: 無検査キャスト*/
+        __featureVector = (HashMap<String, Integer>) tuple.getValue(0);
+        __setWeightVector();
+        __predict();
+
+        collector.emit(new Values(__weightVector));
     }
 
     @Override
@@ -92,5 +103,6 @@ public class PerceptronBolt extends BaseRichBolt {
 
     @Override
     public void declareOutputFields(OutputFieldsDeclarer declarer) {
+        declarer.declare(new Fields("weight_vector"));
     }
 }
